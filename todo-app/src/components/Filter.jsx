b83541f656b3c70fd2f2a77791a8dfddf4be4803
REDOWAN038 <redowanibrahim2724@gmail.com>
2024-05-26 14:18:56 +0600

    complete functionality of addTodo, filter todos and search todos

---
+++ todo-app/src/components/Filter.jsx
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from "react-redux"
+import { filterTodos, markAllCompleted } from "../features/todo/todoSlice"
+
+const Filter = () => {
+    const currentFilter = useSelector((state) => state.todo.value)
+    const dispatch = useDispatch()
+
+    const handleFilter = (filter) => {
+        dispatch(filterTodos(filter))
+    }
+
+    return (
+        <div className='flex space-x-4 items-center'>
+            <select
+                value={currentFilter}
+                onChange={(e) => handleFilter(e.target.value)}
+                className='text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none'
+            >
+                <option value='ALL'>ALL</option>
+                <option value='COMPLETED'>COMPLETED</option>
+                <option value='INCOMPLETE'>INCOMPLETE</option>
+            </select>
+
+            <button
+                onClick={() => dispatch(markAllCompleted())}
+                className='text-sm px-2 py-1 bg-purple-500 text-white ml-2 rounded'
+            >
+                Mark All Completed
+            </button>
+        </div>
+    )
+}
+
+export default Filter
---
+++ todo-app/src/components/Todo.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import { BsPlus, BsSearch } from "react-icons/bs"
+import { useSelector, useDispatch } from "react-redux"
+import { addTodo, searchTodos } from "../features/todo/todoSlice"
+import Filter from "./Filter"
+
+const Todo = () => {
+    const [newTodo, setNewTodo] = useState("")
+    const [search, setSearch] = useState("")
+    const dispatch = useDispatch()
+
+    const handleAddTodo = () => {
+        if (newTodo.trim() !== "") {
+            dispatch(addTodo(newTodo.trim()))
+        }
+    }
+
+    const handleSearchTodo = (value) => {
+        setSearch(value)
+        dispatch(searchTodos(search))
+    }
+
+    return (
+        <div className='max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded'>
+            <h2 className='mt-3 mb-6 text-2xl font-bold text-center uppercase'>
+                personal todo app
+            </h2>
+
+            {/* add todo section */}
+
+            <div className='flex items-center mb-4'>
+                <input
+                    type='text'
+                    placeholder='Add Todo'
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                    className='flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500'
+                />
+                <button
+                    className='ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none'
+                    onClick={handleAddTodo}
+                >
+                    <BsPlus />
+                </button>
+            </div>
+
+            {/* filter and search */}
+
+            <div className='flex items-center justify-between'>
+                <Filter />
+                <div className='flex items-center mb-4'>
+                    <input
+                        type='text'
+                        placeholder='Search Todo'
+                        value={search}
+                        onChange={(e) => handleSearchTodo(e.target.value)}
+                        className='flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500'
+                    />
+                    <button className='ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none'>
+                        <BsSearch />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
